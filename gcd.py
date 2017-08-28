4e1a6652e1fcad8c2f896ab1496492d6bed12edb
milanmenezes <milanmenezes@gmail.com>
2017-08-28 00:26:19 -0400

    gcd

--- gcd.py
+++ gcd.py
@@ -1,10 +1,10 @@
-def gcd(a,b,n):
-  if(a%n==0 and b%n==0):
-    return n
-  return gcd(a,b,n-1)
+def gcd(a,b):
+  if(a==0):
+    return b
+  return gcd(b%a,a)
 
 x=raw_input("Enter 2 numbers\n")
 x=x.split()
 a=eval(x[0])
 b=eval(x[1])
-print "The GCD is: "+ str(gcd(a,b,min(a,b)))
+print "The GCD is: "+ str(gcd(a,b))
--- newton.py
+++ newton.py
@@ -1,13 +1,15 @@
 from math import sqrt
 
+
 def newton(a):
   x=a/2.0
   y=0
-  while(1):
-    y=(x+(a/x))/2.0
+  for t in range(1,3):
+    y=(x+a/x)/2.0
     if(x==y):
       return x
     x=y
+  return x
 
 print "num\t","math.sqrt(num)\t\t","Newton's method"
 for i in range(1,11):
