405035747d31982e73b70e09f872e13f5b2e503f
milanmenezes <milanmenezes@gmail.com>
2017-09-23 03:49:03 -0400

    Emial

---
+++ unit3/email.py
@@ -0,0 +1,23 @@
+import re
+import operator
+f=open("mbox-short.txt")
+l=[]
+dict={}
+for line in f:
+  l.extend(re.findall(r'From: +[\w\.-]+@[\w\.-]+\.\w+', line))
+  l.extend(re.findall(r'From +[\w\.-]+@[\w\.-]+\.\w+', line))
+  
+for i in range(len(l)):
+  l[i]=l[i].replace("From: ","")
+  l[i]=l[i].replace("From ","")
+  
+for i in l:
+  print i
+  if(i in dict):
+    dict[i]+=1
+  else:
+    dict[i]=1
+
+t=tuple(dict.items())
+t = sorted(t, key=operator.itemgetter(1),reverse=True)
+print "The most popular email id is: "+t[0][0]
