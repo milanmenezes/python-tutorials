e1bbc7284c021075423c952690f3326c96e90111
milanmenezes <milanmenezes@gmail.com>
2017-09-23 04:01:06 -0400

    words

---
+++ unit3/count.py
@@ -0,0 +1,12 @@
+dict={"alphabet":0,"digits":0,"punctuation":0}
+t=[]
+def fun(x):
+  x=x.split("\n")
+  for i in x:
+    k=i.split()
+    for j in k:
+      m=j.split("")
+      t.append(m)
+  for i in m:
+    if()
+    
